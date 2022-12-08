38627c81beb648c9679e0ba1dea16145ad156e3d
c0rrosive <michaellabadie@gmail.com>
2022-12-08 01:20:27 -0700

    Create lambda_function.py
    yes

---
+++ lambda_function.py
@@ -0,0 +1,82 @@
+import boto3
+
+stack_name = 'cutdowntest'
+stackset_name = 'cutdownstackset'
+AWS_OU = 'r-emby'
+cloudformation_client = boto3.client('cloudformation')
+
+def get_stack_params():
+    response = cloudformation_client.describe_stacks(
+        StackName= stack_name
+    )
+    return(response['Stacks'][0]['Parameters'])
+
+def get_stackset_params():
+    response = cloudformation_client.describe_stack_set(
+        StackSetName= stackset_name,
+    )
+    return(response['StackSet']['Parameters'])
+
+def update_stack():
+    cloudformation_client.update_stack(
+          StackName=stack_name,
+          TemplateURL='https://redlock-public.s3.amazonaws.com/cft/rl-read-and-write.template',
+          UsePreviousTemplate=False,
+          Parameters=stack_params,
+          DisableRollback=True,
+          Capabilities=[
+            'CAPABILITY_NAMED_IAM'
+        ]
+
+    )
+
+def update_stackset():
+    cloudformation_client.update_stack_set(
+        StackSetName='cutdownstackset',
+        TemplateURL='https://redlock-public.s3.amazonaws.com/cft/rl-read-and-write-member.template',
+        UsePreviousTemplate=False,
+        Parameters=stackset_params,
+        Capabilities=[
+    'CAPABILITY_NAMED_IAM'
+        ],
+
+        OperationPreferences={
+            'RegionConcurrencyType':'PARALLEL',
+            'FailureTolerancePercentage': 100,
+            'MaxConcurrentPercentage': 100
+        },
+
+
+        DeploymentTargets={
+
+            'OrganizationalUnitIds': [
+                AWS_OU,
+            ],
+
+        },
+        PermissionModel='SERVICE_MANAGED',
+        AutoDeployment={
+            'Enabled': True,
+            'RetainStacksOnAccountRemoval': False
+        },
+        Regions=[
+            'us-east-1',
+        ]
+
+    )
+
+stack_params = get_stack_params()
+stackset_params = get_stackset_params()
+
+def lambda_handler(event, context):
+    update_stack()
+    update_stackset()
+
+
+
+
+
+
+
+
+
